42c155f8f39d51bdb68dc4395c3c905c601a4b74
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-11-27 14:34:14 -0800

    Change db connection

--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,29 @@
 // PG database client/connection setup
-const { Pool } = require("pg");
+// const { Pool } = require("pg");
 
-const dbParams = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
-};
+// const dbParams = {
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME
+// };
 
-const db = new Pool(dbParams);
+// const db = new Pool(dbParams);
 
-db.connect();
+// db.connect();
 
-module.exports = db;
+// module.exports = db;
+
+const pg = require("pg");
+
+const client = new pg.Client({
+  connectionString: process.env.DATABASE_URL || "",
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
+});
+
+client
+  .connect()
+  .catch((e) => console.log(`Error connecting to Postgres server:\n${e}`));
+
+module.exports = client;
