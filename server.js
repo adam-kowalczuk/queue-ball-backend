c42fe2c203c67c3f321edaf30f56f8c84d8e6968
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-11-27 15:19:59 -0800

    Update websocket URL

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const http = require("http");
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:8080/"
+    origin: "https://queue-ball-backend-production.up.railway.app"
   }
 });
 
